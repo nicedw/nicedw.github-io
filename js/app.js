99db5271e1d01562740750f4e28534e5802b3da2
Nick <nicedw@hotmail.com>
2016-05-25 10:28:40 +1200

    2
    2

--- js/app.js
+++ js/app.js
@@ -1,91 +1,54 @@
+//page 1
+$(function() { //nick: check this is the same syntax as document.ready
+    $('#orderqty').change(function() {
+        calculateCart();
+    });
+
+    $('#nextButton').click(function() {
+        var data = { // creating an object literal
+            "Name": $('#ordername').val(),
+            "Email": $('#orderemail').val(),
+            "Price": $('#orderprice').attr('data-value'),
+            "Qty": $('#orderqty').val(),
+            "Ext": $('#orderext').text(),
+            "Desc": $('#orderdesc').text(),
+            "Id": $('#orderid').text()
+        };
+
+        var session = window.sessionStorage,
+            dataString = JSON.stringify(data);
+        console.log(dataString);
+
+        session.setItem("cart", dataString);
+        showPageTwo();
+    });
+
+    function calculateCart() {
+        var price = parseFloat($('#orderprice').attr('data-value')),
+            qty = parseInt($('#orderqty').val()),
+            ext = parseFloat(price * qty);
+
+        $('#orderext').html(ext.toFixed(2));
+    }
+
+    calculateCart();
 
 
 
-// $( "#questions" ).submit(function( event ) {
-// 	if (	$( "input[name='drink']" ).val() === "correct"    ){
-// 		alert("correct!");
-// 	}
-
-//     // $( "span" ).text( "Validated..." ).show();
-  
-// });
+    //page2
 
 
-// $( "#questions" ).submit(function( event ) {
-// 	$( "input[name='drink2']" ).val($( "input[name='drink']" ).val()) ;
-// });
+    function showPageTwo() {
+        alert('showPageTwo');
+        var session = window.sessionStorage,
+            data = JSON.parse(session.getItem('cart'));
 
-
-
-
-$( "#page0copy" ).click (function() {
-	
-	
-
-	// Copy all the user's inputs to the formspree form
-	$( "input[name='usersname']" ).val($( "input[name='usernameinput']" ).val()) ;
-
-	// Clear page0 then display page1
-	$( "#page0").hide();   
-	$( "#page1").show();
-
-
-	// Display the user's name on page1
-	 var name = $("input[name='usersname']" ).val();
-	 $( "#page1" ).prepend("<h2>Welcome "+ name + "</h2>");
+        $('#summaryname').text(data.Name);
+        $('#summaryemail').text(data.Email);
+        $('#summaryprice').text(data.Price);
+        $('#summaryqty').text(data.Qty);
+        $('#summaryext').text(data.Ext);
+        $('#summarydesc').text(data.Desc);
+        $('#summaryid').text(data.Id);
+    }
 });
-
-
-
-$( "#page1copy" ).click (function() {
-	
-	// Copy all the user's inputs to the formspree form. Check the final state of the checkboxes and copy to the formspree form.
-	if ($('#userlatte').is(':checked')){
-		$('#latte').prop('checked', true);
-	}
-
-	if ($('#usercappuccino').is(':checked')){
-		$('#cappuccino').prop('checked', true);
-	}
-
-
-	if ($('#usergreentea').is(':checked')){
-		$('#greentea').prop('checked', true);
-	}
-
-
-
-
-	
-	// Clear page 1 then display page 2 - to do : make this into a seperate function to call
-	$( "#page1").hide();
-	$( "#page2").show();
-});
-
-
-$( "#finishbutton" ).click(function() {
-// Copy all the user's inputs to the formspree form
-
-// to do
-$( "input[name='comments']" ).val($( "input[name='usercomments']" ).val()) ;
-$( "input[name='food']" ).val($( "input[name='userfood']" ).val()) ;
-
-
-
-
-// submit the form when the finish button is pressed
-  $( "#submitform" ).submit();
-});
-
-
-
-
-$( "#anotherorder" ).click (function() {
-
- 	window.location = "index.html";
-
-
-});
-
-
-
